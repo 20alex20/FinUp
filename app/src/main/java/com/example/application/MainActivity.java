be1e1c2ef5ae22358cd57a7b225307d5218bbdb2
alex <mike123777mike@gmail.com>
2022-01-28 16:14:24 +0300

    Подключил библиотеку matplotlip и вывел тестовый график

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText Et1, Et2;
     Button Btn;
     TextView tv;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         Et2 = (EditText)findViewById(R.id.et2);
         Btn = (Button)findViewById(R.id.btn);
         tv = (TextView)findViewById(R.id.text_view);
+        iv = (ImageView)findViewById(R.id.imageView2);
+        iv.setVisibility(View.INVISIBLE);
 
         if(!Python.isStarted())
             Python.start(new AndroidPlatform(this));
@@ -38,8 +46,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 PyObject obj = pyobj.callAttr("main", Et1.getText().toString(), Et2.getText().toString());
-
-                tv.setText(obj.toString());
+                byte[] ba = obj.toJava(byte[].class);
+                iv.setVisibility(View.VISIBLE);
+                iv.setImageBitmap(BitmapFactory.decodeByteArray(ba,0, ba.length));
+                // tv.setText(obj.toString());
             }
         });
     }
--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,6 +1,28 @@
+import matplotlib.pyplot as plt
+import io
+
+
 def main(number1, number2):
+
     num1 = int(number1)
     num2 = int(number2)
+    # summ = num1 + num2
 
-    summ = num1 + num2
-    return "Сумма: " + str(summ)
+    xa = [float(word) for word in range(num1)]
+    ya = [float(word) for word in range(num2)]
+    fig, ax = plt.subplots()
+    ax.plot(xa, ya)
+    f = io.BytesIO()
+    plt.savefig(f, format="png")
+    return f.getvalue()
+
+    #plt.style.use('_mpl-gallery')
+    #x = np.linspace(0, 10, 100)
+    #y = 4 + 2 * np.sin(2 * x)
+    #fig, ax = plt.subplots()
+    #ax.plot(x, y, linewidth=2.0)
+    #ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+    #       ylim=(0, 8), yticks=np.arange(1, 8))
+    #plt.savefig('../res/drawable/img.png')
+
+    #return "Сумма: " + str(summ)
