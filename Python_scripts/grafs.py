33bce2c176c603c1d8a0156d8e4a66adc07a622f
Alex <mike123777mike@gmail.com>
2022-02-18 18:00:19 +0300

    Последний коммит перед мёрджем

--- Python_scripts/grafs.py
+++ Python_scripts/grafs.py
@@ -4,7 +4,7 @@ import numpy as np
 from main import DB
 
 
-def f(time_mode: int, args, db):
+def f(type: bool, time_mode: int, args, db):
     categories = db.get_categories()
     labels = []
     sums = []
@@ -12,7 +12,7 @@ def f(time_mode: int, args, db):
         cursum = 0
         date_start = dt.date(*(list(args[:time_mode]) + [1] * (3 - time_mode)))
         date_stop = dt.date(*(list(args[time_mode:]) + [1] * (3 - time_mode)))
-        for id_purchase, sum, date in db.get_purchase2(id_category):
+        for id_purchase, sum, date in db.get_purchase_deposit(type, id_category):
             if date_start <= dt.date(*map(int, date.split('.')[::-1])) < date_stop:
                 cursum += sum
         labels.append(name)
@@ -33,7 +33,7 @@ def dn(time_mode, date_start):
     return date_start
 
 
-def f2(time_mode: int, args, db):
+def f2(type: bool, time_mode: int, args, db):
     labels = []
     cs = {}
     categories = db.get_categories()
@@ -47,19 +47,19 @@ def f2(time_mode: int, args, db):
         labels.append('.'.join(date_start.strftime("%d.%m.%Y").split('.')[3 - time_mode:]))
         arggs = [date_start.year, date_start.month, date_start.day][:time_mode] + \
                 [date_next.year, date_next.month, date_next.day][:time_mode]
-        for label, sum in zip(*f(time_mode, arggs, db)):
+        for label, sum in zip(*f(type, time_mode, arggs, db)):
             cs[label].append(sum)
         date_start = date_next
         date_next = dn(time_mode, date_start)
     return cs, labels
 
 
-def graph(mode: str, time_mode: int, *args):  # year_start, month_start, day_start, year_stop, month_stop, day_stop
-    db = DB("../db.sqlite3")
+def graph(type: bool, mode: str, time_mode: int, *args):  # year_start, month_start, day_start, year_stop, month_stop, day_stop
+    db = DB("db.sqlite3")
     print(db.login('jj', 'kk'))
     fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
     if mode == "plot":
-        cs, labels = f2(time_mode, args, db)
+        cs, labels = f2(type, time_mode, args, db)
         for category, sums in cs.items():
             ax.plot(np.arange(len(sums)), sums, label=category)
             ax.set_xticks(np.arange(len(sums)), labels=labels)
@@ -69,7 +69,7 @@ def graph(mode: str, time_mode: int, *args):  # year_start, month_start, day_sta
         ax.set_title("График расходов за выбранный период")
         ax.legend()
     elif mode == 'pie':
-        labels, sums = f(time_mode, args, db)
+        labels, sums = f(type, time_mode, args, db)
         colors = plt.get_cmap('Oranges')(np.linspace(0.2, 0.7, len(labels)))
         ax.pie(sums, colors=colors, radius=3, center=(4, 4), labels=labels,
                wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True, autopct='%1.1f%%', shadow=True)
@@ -77,7 +77,7 @@ def graph(mode: str, time_mode: int, *args):  # year_start, month_start, day_sta
         ax.set(xlim=(0, 8), xticks=range(1, 1), ylim=(0, 8), yticks=range(1, 1))
         ax.axis('off')
     elif mode == 'bar':
-        labels, sums = f(time_mode, args, db)
+        labels, sums = f(type, time_mode, args, db)
         x = np.arange(len(labels))
         width = 0.35
         fig, ax = plt.subplots()
@@ -88,7 +88,7 @@ def graph(mode: str, time_mode: int, *args):  # year_start, month_start, day_sta
         ax.set_title('Cуммы по категориям')
         ax.bar_label(rects2, padding=3)
     elif mode == 'bar2':
-        cs, labels = f2(time_mode, args, db)
+        cs, labels = f2(type, time_mode, args, db)
         ind = np.arange(len(tuple(cs.values())[0]))
         width = 0.35
         last = None
@@ -108,4 +108,4 @@ def graph(mode: str, time_mode: int, *args):  # year_start, month_start, day_sta
     return
 
 
-graph("plot", 2, 2020, 4, 2020, 6)
+graph(True, "bar2", 2, 2020, 4, 2020, 6)
--- Python_scripts/main.py
+++ Python_scripts/main.py
@@ -23,6 +23,7 @@ class DB:
         self.edit_about_me_query = 'UPDATE users SET username_email="{username_email}", full_name="{full_name}" ' \
                                    'WHERE id_user={id_user}'
         self.delete_my_account_query = 'DELETE FROM users WHERE id_user={id_user}'
+
         self.add_category_query = 'INSERT INTO categories(name, id_user, description) ' \
                                   'VALUES("{name}", {id_user}, "{description}")'
         self.get_categories_query = 'SELECT id_category, name, description FROM ' \
@@ -31,15 +32,59 @@ class DB:
         self.edit_category_query = 'UPDATE categories SET name="{name}", description="{description}" ' \
                                    'WHERE id_category={id_category}'
         self.delete_category_query = 'DELETE FROM categories WHERE id_category={id_category}'
-        self.add_purchase_query = 'INSERT INTO purchases(date_time_add, id_category, sum, date, comment) ' \
-                                  'VALUES("{date_time_add}", {id_category}, {sum}, "{date}", "{comment}")'
-        self.get_purchases_query = 'SELECT purchases.id_purchase, purchases.id_category, purchases.sum, ' \
-                                   'purchases.date, purchases.comment FROM purchases INNER JOIN categories ' \
-                                   'ON purchases.id_category = categories.id_category ' \
+
+        self.add_deposit_category_query = 'INSERT INTO deposit_categories(name, id_user, description) ' \
+                                          'VALUES("{name}", {id_user}, "{description}")'
+        self.get_deposit_categories_query = 'SELECT id_deposit_category, name, description FROM ' \
+                                            'deposit_categories WHERE id_user={id_user}'
+        self.is_there_deposit_category = 'SELECT id_deposit_category FROM deposit_categories WHERE name="{name}", ' \
+                                         'id_user={id_user}'
+        self.edit_deposit_category_query = 'UPDATE deposit_categories SET name="{name}", description="{description}" ' \
+                                           'WHERE id_deposit_category={id_deposit_category}'
+        self.delete_deposit_category_query = 'DELETE FROM deposit_categories WHERE ' \
+                                             'id_deposit_category={id_deposit_category}'
+
+        self.add_purchase_query = 'INSERT INTO purchases(date_time_add, id_category, id_bank_account, sum, date, ' \
+                                  'comment) VALUES("{date_time_add}", {id_category}, {id_bank_account}, {sum}, ' \
+                                  '"{date}", "{comment}")'
+        self.get_purchases_query = 'SELECT purchases.id_purchase, purchases.id_category, purchases.id_bank_account, ' \
+                                   'purchases.sum, purchases.date, purchases.comment FROM purchases ' \
+                                   'INNER JOIN categories ON purchases.id_category = categories.id_category ' \
                                    'AND categories.id_user={id_user}'
-        self.edit_purchase_query = 'UPDATE purchases SET sum={sum}, date="{date}", comment="{comment}"' \
-                                   'WHERE id_purchase={id_purchase}'
+        self.get_sum_purchase_query = 'SELECT sum FROM purchases WHERE purchase={purchase}'
+        self.get_purchases_query2 = 'SELECT id_purchase, sum, date FROM ' \
+                                    'purchases WHERE id_category={id_category}'
+        self.edit_purchase_query = 'UPDATE purchases SET id_category={id_category}, id_bank_account={id_bank_account}' \
+                                   ' sum={sum}, date="{date}", comment="{comment}" WHERE id_purchase={id_purchase}'
         self.delete_purchase_query = 'DELETE FROM purchases WHERE id_purchase={id_purchase}'
+
+        self.add_bank_account_query = 'INSERT INTO bank_accounts(name, id_user, current_sum, description) ' \
+                                      'VALUES("{name}", {id_user}, {current_sum}, "{description}")'
+        self.get_bank_accounts_query = 'SELECT id_bank_account, name, current_sum, description FROM ' \
+                                       'bank_accounts WHERE id_user={id_user}'
+        self.is_there_bank_account = 'SELECT id_bank_account FROM bank_accounts WHERE name="{name}", id_user={id_user}'
+        self.get_current_sum_query = 'SELECT current_sum FROM bank_accounts WHERE id_bank_account={id_bank_account}'
+        self.edit_sum_query = 'UPDATE bank_accounts SET current_sum={current_sum} ' \
+                              'WHERE id_bank_account={id_bank_account}'
+        self.edit_bank_account_query = 'UPDATE bank_accounts SET name="{name}", description="{description}" ' \
+                                       'current_sum={current_sum} WHERE id_bank_account={id_bank_account}'
+        self.delete_bank_account_query = 'DELETE FROM bank_accounts WHERE id_bank_account={id_bank_account}'
+
+        self.add_deposit_query = 'INSERT INTO deposits(date_time_add, id_deposit_category, id_bank_account, sum,' \
+                                 'date, comment) VALUES("{date_time_add}", {id_deposit_category}, {id_bank_account}, ' \
+                                 '{sum}, "{date}", "{comment}")'
+        self.get_deposits_query = 'SELECT deposits.id_deposit, deposits.id_deposit_category, deposits.id_bank_account,' \
+                                  ' deposits.sum, deposits.date, deposits.comment FROM deposits ' \
+                                  'INNER JOIN deposit_categories ON deposits.id_deposit_category = ' \
+                                  'deposit_categories.id_deposit_category AND deposit_categories.id_user={id_user}'
+        self.get_sum_deposit_query = 'SELECT sum FROM deposits WHERE deposit={deposit}'
+        self.get_deposits_query2 = 'SELECT id_deposit, sum, date FROM ' \
+                                   'deposits WHERE id_deposit_category={id_deposit_category}'
+        self.edit_deposit_query = 'UPDATE deposits SET id_deposit_category={id_deposit_category}, ' \
+                                  'id_bank_account={id_bank_account} sum={sum}, date="{date}", comment="{comment}" ' \
+                                  'WHERE id_deposit={id_deposit}'
+        self.delete_deposit_query = 'DELETE FROM deposits WHERE id_deposit={id_deposit}'
+
         self.id_user = None
         self.full_name = None
         self.username_email = None
@@ -72,6 +117,7 @@ class DB:
         connection.commit()
         connection.close()
         return "OK"
+
 
     def register(self, username_email, password, full_name):
         ans = self.get_data(format(self.is_there_username_email, username_email))
@@ -116,6 +162,7 @@ class DB:
         except Exception as e:
             raise Exception("Аккаунт удалён")
 
+
     def add_category(self, name, description):
         ans = self.get_data(format(self.is_there_category, name, self.id_user))
         if ans:
@@ -137,19 +184,107 @@ class DB:
         self.do_query(format(self.delete_category_query, id_category))
         raise Exception("Категория удалена")
 
-    def add_purchase(self, id_category, sum, date, comment):
+
+    def add_deposit_category(self, name, description):
+        ans = self.get_data(format(self.is_there_deposit_category, name, self.id_user))
+        if ans:
+            return "Категория с таким названием уже существует"
+        self.do_query(format(self.add_deposit_category_query, name, self.id_user, description))
+        return "Данные изменены"
+
+    def get_deposit_categories(self):
+        return self.get_data(format(self.get_deposit_categories_query, self.id_user))
+
+    def edit_deposit_category(self, id_deposit_category, name, description):
+        ans = self.get_data(format(self.is_there_deposit_category, name, self.id_user))
+        if ans and ans[0][0] != id_deposit_category:
+            return "Категория с таким названием уже существует"
+        self.do_query(format(self.edit_deposit_category_query, name, description, id_deposit_category))
+        return "Данные изменены"
+
+    def delete_deposit_category(self, id_deposit_category):
+        self.do_query(format(self.delete_deposit_category_query, id_deposit_category))
+        raise Exception("Категория удалена")
+
+
+    def add_purchase(self, id_category, id_bank_account, sum, date, comment):
+        ans = self.get_data(format(self.get_current_sum_query, id_bank_account))[0][0]
+        if ans < sum:
+            return "Средств недостаточно"
         self.do_query(format(self.add_purchase_query, dt.now().strftime("%Y.%m.%d %H:%M:%S"),
-                             id_category, sum, date, comment))
+                             id_category, id_bank_account, sum, date, comment))
+        self.do_query(format(self.edit_sum_query, id_bank_account, ans - sum))
         return "Данные изменены"
 
     def get_purchase(self):
         return self.get_data(format(self.get_purchases_query, self.id_user))
 
-    def edit_purchase(self, id_purchase, sum, date, comment):
-        self.do_query(format(self.add_purchase_query, sum, date, comment, id_purchase))
+
+    def edit_purchase(self, id_purchase, id_category, id_bank_account, sum, date, comment):
+        ans = self.get_data(format(self.get_current_sum_query, id_bank_account))[0][0]
+        ans2 = self.get_data(format(self.get_sum_purchase_query, id_purchase))[0][0]
+        if ans < sum - ans2:
+            return "Средств недостаточно"
+        self.do_query(format(self.add_purchase_query, id_category, id_bank_account, sum, date, comment, id_purchase))
+        self.do_query(format(self.edit_sum_query, id_bank_account, ans - (sum - ans2)))
         return "Данные изменены"
 
     def delete_purchase(self, id_purchase):
         self.do_query(format(self.add_purchase_query, id_purchase))
         return "Данные изменены"
 
+
+    def add_bank_account(self, name, current_sum, description):
+        ans = self.get_data(format(self.is_there_category, name, self.id_user))
+        if ans:
+            return "Категория с таким названием уже существует"
+        self.do_query(format(self.add_category_query, name, self.id_user, current_sum, description))
+        return "Данные изменены"
+
+    def get_bank_accounts(self):
+        return self.get_data(format(self.get_categories_query, self.id_user))
+
+    def edit_bank_account(self, id_category, name, current_sum, description):
+        ans = self.get_data(format(self.is_there_category, name, self.id_user))
+        if ans and ans[0][0] != id_category:
+            return "Категория с таким названием уже существует"
+        self.do_query(format(self.edit_category_query, name, current_sum, description, id_category))
+        return "Данные изменены"
+
+    def delete_bank_account(self, id_category):
+        self.do_query(format(self.delete_category_query, id_category))
+        raise Exception("Категория удалена")
+
+
+    def add_deposit(self, id_deposit_category, id_bank_account, sum, date, comment):
+        ans = self.get_data(format(self.get_current_sum_query, id_bank_account))[0][0]
+        if ans < sum:
+            return "Средств недостаточно"
+        self.do_query(format(self.add_deposit_query, dt.now().strftime("%Y.%m.%d %H:%M:%S"),
+                             id_deposit_category, id_bank_account, sum, date, comment))
+        self.do_query(format(self.edit_sum_query, id_bank_account, ans - sum))
+        return "Данные изменены"
+
+    def get_deposits(self):
+        return self.get_data(format(self.get_deposits_query, self.id_user))
+
+    def edit_deposit(self, id_deposit, id_deposit_category, id_bank_account, sum, date, comment):
+        ans = self.get_data(format(self.get_current_sum_query, id_bank_account))[0][0]
+        ans2 = self.get_data(format(self.get_sum_deposit_query, id_deposit))[0][0]
+        if ans < ans2 - sum:
+            return "Средств недостаточно"
+        self.do_query(format(self.add_deposit_query, id_deposit_category, id_bank_account, sum, date, comment,
+                             id_deposit))
+        self.do_query(format(self.edit_sum_query, id_bank_account, ans - (ans2 - sum)))
+        return "Данные изменены"
+
+    def delete_deposit(self, id_deposit):
+        self.do_query(format(self.add_deposit_query, id_deposit))
+        return "Данные изменены"
+
+
+    def get_purchase_deposit(self, type, id):
+        if type:
+            return self.get_data(format(self.get_purchases_query2, id))
+        else:
+            return self.get_data(format(self.get_deposits_query2, id))
