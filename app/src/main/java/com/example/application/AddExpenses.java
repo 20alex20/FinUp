a3822b818d92886a30d6090dc33ce31fae0668fd
Dmitry <183646@gmail.com>
2022-03-18 15:30:10 +0300

    Календарь

--- app/src/main/java/com/example/application/AddExpenses.java
+++ app/src/main/java/com/example/application/AddExpenses.java
@@ -8,30 +8,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddExpenses extends AppCompatActivity {
 
     CalendarView calendar;
     EditText Et1, Et2, Et3;
     Button Btn;
-    int mYear, mMonth, mDay;
+    ImageView cls;
+    TextInputLayout list1, list2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
 
-
-        // Et1 = (EditText)findViewById(R.id.email_reg);
-        // Et2 = (EditText)findViewById(R.id.username);
-        // Et3 = (EditText)findViewById(R.id.PASSword);
+        Et1 = (EditText)findViewById(R.id.editTextBillName);
+        Et2 = (EditText)findViewById(R.id.editTextBillName2);
+        Et3 = (EditText)findViewById(R.id.editTextBillName3);
+        cls = (ImageView)findViewById(R.id.imageView16);
         Btn = (Button)findViewById(R.id.button);
-        calendar = findViewById(R.id.calendarView);
+        calendar = (CalendarView)findViewById(R.id.calendarView);
+        list1 = (TextInputLayout)findViewById(R.id.editTextBillCateg);
+        list2 = (TextInputLayout)findViewById(R.id.editTextBillCateg2);
+
 
         if(!Python.isStarted())
             Python.start(new AndroidPlatform(this));
@@ -39,23 +45,25 @@ public class AddExpenses extends AppCompatActivity {
         Python py = Python.getInstance();
         final PyObject pyobj = py.getModule("main");
 
+        PyObject obj = pyobj.callAttr("get_categories");
+        String[][] array = obj.toJava(String[][].class);
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                mYear = year;
-                mMonth = month + 1;
-                mDay = dayOfMonth;
-                String selectedDate = new StringBuilder().append(mMonth + 1)
-                        .append("-").append(mDay).append("-").append(mYear)
-                        .append(" ").toString();
-                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+                int mYear = year;
+                int mMonth = month + 1;
+                int mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mMonth)
+                        .append(".").append(mDay).append(".").append(mYear).toString();
+                Et3.setText(selectedDate);
             }
         });
 
         Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PyObject obj = pyobj.callAttr("register", Et1.getText().toString(), Et3.getText().toString(), Et2.getText().toString());
+                PyObject obj = pyobj.callAttr("add_deposit", Et1.getText().toString(), Et3.getText().toString(), Et2.getText().toString());
                 String s = obj.toString();
                 Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
                 goBack(view);
@@ -69,13 +77,13 @@ public class AddExpenses extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goBack(View view) {
-        Intent intent = new Intent(this, OperationsActivity.class);
-        startActivity(intent);
+    public void open(View view) {
+        cls.setVisibility(ImageView.VISIBLE);
+        calendar.setVisibility(CalendarView.VISIBLE);
     }
 
-    public void goBack(View view) {
-        Intent intent = new Intent(this, OperationsActivity.class);
-        startActivity(intent);
+    public void close(View view) {
+        cls.setVisibility(ImageView.INVISIBLE);
+        calendar.setVisibility(CalendarView.INVISIBLE);
     }
 }
