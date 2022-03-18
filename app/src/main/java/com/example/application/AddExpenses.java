cce965f960e8fef6d133722cb6da14d0319b188e
Dmitry <183646@gmail.com>
2022-03-18 11:22:12 +0300

    Добавил календарь

--- app/src/main/java/com/example/application/AddExpenses.java
+++ app/src/main/java/com/example/application/AddExpenses.java
@@ -5,13 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class AddExpenses extends AppCompatActivity {
+
+    CalendarView calendar;
+    EditText Et1, Et2, Et3;
+    Button Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
+
+
+        // Et1 = (EditText)findViewById(R.id.email_reg);
+        // Et2 = (EditText)findViewById(R.id.username);
+        // Et3 = (EditText)findViewById(R.id.PASSword);
+        Btn = (Button)findViewById(R.id.button);
+        calendar = findViewById(R.id.calendarView);
+
+        if(!Python.isStarted())
+            Python.start(new AndroidPlatform(this));
+
+        Python py = Python.getInstance();
+        final PyObject pyobj = py.getModule("main");
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mMonth + 1)
+                        .append("-").append(mDay).append("-").append(mYear)
+                        .append(" ").toString();
+                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PyObject obj = pyobj.callAttr("register", Et1.getText().toString(), Et3.getText().toString(), Et2.getText().toString());
+                String s = obj.toString();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                goBack(view);
+                // tv.setText(obj.toString());
+            }
+        });
     }
 
     public void goBack(View view) {
