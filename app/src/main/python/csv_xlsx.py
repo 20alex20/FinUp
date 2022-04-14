9e8345351bd7de4165d9f45fe72ad9f779a3bba6
Alex <mike123777mike@gmail.com>
2022-04-14 14:18:05 +0300

    export

--- app/src/main/python/csv_xlsx.py
+++ app/src/main/python/csv_xlsx.py
@@ -2,11 +2,22 @@ import csv
 import xlsxwriter
 
 
-def export_csv(directory: str, headers: list, cur_list: list):
-    with open(directory + '/export_data_FinUp.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(headers)
-        writer.writerows(cur_list)
+headers2 = {
+    "categories": ("id_category", "name", "description"),
+    "deposit_categories": ("id_deposit_category", "name", "description"),
+    "purchases": ("id_purchase", "sum", "date"),
+    "bank_accounts": ("id_bank_account", "name", "current_sum", "description"),
+    "deposits":  ("id_deposit", "sum", "date")
+}
+
+def export_csv(directory: str):
+
+    for name in sorted(headers2):
+        headers = headers2[name]
+        with open(directory + f'/export_data_FinUp_{name}.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(headers)
+            writer.writerows(cur_list)
 
 
 def export_xlsx(directory: str, headers: list, cur_list: list):  # directory - Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
